e016043085e7b20c74a83c2474bdf7dfb934ca18
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2023-07-04 01:24:47 -0300

    migracion

--- BarrioPrivado.BD/Migrations/20230704042422_relaciones.Designer.cs
+++ BarrioPrivado.BD/Migrations/20230704042422_relaciones.Designer.cs
@@ -3,6 +3,7 @@ using BarrioPrivado.BD.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BarrioPrivado.BD.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230704042422_relaciones")]
+    partial class relaciones
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BarrioPrivado.BD/Migrations/20230704042422_relaciones.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarrioPrivado.BD.Migrations
+{
+    /// <inheritdoc />
+    public partial class relaciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Domicilios_ResidenteId",
+                table: "Domicilios");
+
+            migrationBuilder.CreateIndex(
+                name: "Domicilio_ResidenteId_UQ",
+                table: "Domicilios",
+                column: "ResidenteId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "Domicilio_ResidenteId_UQ",
+                table: "Domicilios");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Domicilios_ResidenteId",
+                table: "Domicilios",
+                column: "ResidenteId");
+        }
+    }
+}
